141248876d14e5f2c13c65aa86a5d244398a70eb
text <manaeffrom@yandex.ru>
2025-01-15 20:32:01 +0400

    fix music and setting panel on scroll for card

--- scroll-about.js
+++
@@ -1,8 +0,0 @@
-// window.addEventListener('scroll', function () {
-//     var scrollTopButton = document.querySelector('.about-box');
-//     if (this.window.pageYOffset > 350) {
-//         scrollTopButton.style.filter='opacity(1)';
-//     } else {
-//         scrollTopButton.style.filter='opacity(0)';
-//     }
-// });
--- scroll-pnl.js
+++ scroll-pnl.js
@@ -1,11 +1,11 @@
 window.addEventListener('scroll', function () {
     var scrollTopButton = document.querySelector('.panel-setting');
-    if (this.window.pageYOffset > 150) {
+    if (this.window.pageYOffset > 10) {
         scrollTopButton.style.width = '100%';
         scrollTopButton.style.marginLeft = '0%';
         scrollTopButton.style.marginTop = '0%';
         scrollTopButton.style.borderRadius = '0px';
-        scrollTopButton.style.height='4rem';
+        scrollTopButton.style.height='3.5rem';
         scrollTopButton.style.boxShadow='0 5px 10px var(--panel-bg-color)'
         scrollTopButton.style.filter='opacity(0.90)'
     } else {
