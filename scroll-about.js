cc8475253575656b272026dec99047742849bd06
text <manaeffrom@yandex.ru>
2025-01-15 06:35:48 +0400

    fix

---
+++ scroll-about.js
@@ -0,0 +1,8 @@
+// window.addEventListener('scroll', function () {
+//     var scrollTopButton = document.querySelector('.about-box');
+//     if (this.window.pageYOffset > 350) {
+//         scrollTopButton.style.filter='opacity(1)';
+//     } else {
+//         scrollTopButton.style.filter='opacity(0)';
+//     }
+// });
--- scroll-pnl.js
+++ scroll-pnl.js
@@ -1,16 +1,20 @@
 window.addEventListener('scroll', function () {
     var scrollTopButton = document.querySelector('.panel-setting');
-    if (this.window.pageYOffset > 0) {
+    if (this.window.pageYOffset > 150) {
         scrollTopButton.style.width = '100%';
         scrollTopButton.style.marginLeft = '0%';
         scrollTopButton.style.marginTop = '0%';
         scrollTopButton.style.borderRadius = '0px';
         scrollTopButton.style.height='4rem';
+        scrollTopButton.style.boxShadow='0 5px 10px var(--panel-bg-color)'
+        scrollTopButton.style.filter='opacity(0.90)'
     } else {
         scrollTopButton.style.width = '50%';
         scrollTopButton.style.marginLeft = '25%';
         scrollTopButton.style.marginTop = '1%';
-        scrollTopButton.style.borderRadius = '20px';
+        scrollTopButton.style.borderRadius = '12px';
         scrollTopButton.style.height='3rem';
+        scrollTopButton.style.boxShadow='0 0px 5px var(--panel-bg-color)'
+         scrollTopButton.style.filter='opacity(1)'
     }
 });
