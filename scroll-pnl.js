c8843ca4a095910e1dc08ab6a806ce7b72bdb758
faska <124354826+user-bat@users.noreply.github.com>
2025-05-09 06:18:12 +0400

    Update scroll-pnl.js

--- scroll-pnl.js
+++ scroll-pnl.js
@@ -1,20 +1,20 @@
 window.addEventListener('scroll', function () {
     var scrollTopButton = document.querySelector('.panel-setting');
-    if (this.window.pageYOffset > 25) {
+    if (this.window.pageYOffset > 40) {
         scrollTopButton.style.width = '100%';
         scrollTopButton.style.marginLeft = '0%';
         scrollTopButton.style.marginTop = '0%';
         scrollTopButton.style.borderRadius = '0px';
-        scrollTopButton.style.height='3rem';
+        scrollTopButton.style.height='2.75rem';
         scrollTopButton.style.boxShadow='0 5px 10px var(--panel-bg-color)'
         scrollTopButton.style.filter='opacity(0.90)'
 
     } else {
-        scrollTopButton.style.width = '50%';
+        scrollTopButton.style.width = '75%';
         scrollTopButton.style.marginLeft = '25%';
         scrollTopButton.style.marginTop = '1%';
         scrollTopButton.style.borderRadius = '15px';
-        scrollTopButton.style.height='2.75rem';
+        scrollTopButton.style.height='2.5rem';
         scrollTopButton.style.boxShadow='0 0px 5px var(--panel-bg-color)'
          scrollTopButton.style.filter='opacity(1)'
 
